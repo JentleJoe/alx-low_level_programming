7df35afdb2af1c1aedf0baf819c230ded7ce1d36
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-03-17 04:22:39 +0000

    added new file

---
+++ 5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0, if successful
+ */
+int main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", string);
+}
