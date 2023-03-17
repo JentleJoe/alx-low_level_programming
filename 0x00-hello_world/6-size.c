0be02098336459d0201f889a15f4591fca66557a
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-03-17 09:32:44 +0000

    created new folder and transferred files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0, if successful
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("%lu\n",
+
+
