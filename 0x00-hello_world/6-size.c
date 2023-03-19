980ec976734eb0885c8adaac31d5db2c6e3825ea
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-03-19 20:57:24 +0000

    changed file permissions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("%lu\n",
+	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of a int: %d yte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %f byte(s)\n", sizeof(f));
 
-
+	return (0);
+}
