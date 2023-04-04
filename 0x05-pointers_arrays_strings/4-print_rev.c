12431bf084a66b3209db2eb458314364f74564e7
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-04-04 12:06:19 +0000

    simplified source code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,21 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int i, j, len;
-
-	i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
+	while (i >= 0)
 	{
-		_putchar(s[j]);
+		_putchar(s[i]);
+		i--;
 	}
-
-	_putchar('\n');
 }
