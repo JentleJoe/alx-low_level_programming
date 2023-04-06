675c546bc2fb95df4baea61fe1d61732f3a42f09
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-04-06 22:23:08 +0000

    replaced loops with recurssive functions

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* prototype of personal function that print string from end */
+void print_rev(char *p);
+
+int _pow_recursion(int x, int y);
 int factorial(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
