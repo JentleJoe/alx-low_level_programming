c3f05628986a8aa16bf0cea4beb0d49df4881c27
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-04-06 04:20:04 +0000

    file updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,13 +9,12 @@ void _print_rev_recursion(char *s)
 {
 	int i;
 
-	while (*s[i + 1] != '\0')
+	while (s[i + 1] != '\0')
 		i++;
-	
+
 	while (i >= 0)
 	{
 		_putchar(s[i]);
 		i--;
 	}
-	_putchar('\n');
 }
