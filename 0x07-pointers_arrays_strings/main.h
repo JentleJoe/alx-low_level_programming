5f4c39e00e847240c22598b98cd055ffbd383217
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-04-24 05:17:43 +0000

    fixed bug in header

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef POINTERS_ARRAYS
 #define POINTERS_ARRAYS
 
-char *_memset(char *s, char b, unsigned int n);A
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
