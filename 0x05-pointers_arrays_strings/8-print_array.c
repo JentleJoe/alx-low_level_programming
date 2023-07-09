c271458aaacaa3fdbe06eeb90ef4597e73e82d25
= <=>
2023-07-09 22:18:39 +0000

    updated source code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == 0)
-			printf("%d", a[i]);
+		if (i + 1 == n)
+		{
+			printf("%d\n", a[i]);
+				break;
+		}
 		else
-			printf(", %d", a[i]);
+		{
+			printf("%d, ", a[i]);
+		}
 	}
-		printf("\n");
 }
