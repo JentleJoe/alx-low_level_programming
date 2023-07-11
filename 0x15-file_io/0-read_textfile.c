581880edf9dc9f76f0280e3a52622dea4852dacd
= <=>
2023-07-11 03:25:58 +0000

    added new files on file i_o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t len;
+	ssize_t lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (len == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	lenw = write(STDOUT_FILENO, buffer, len);
+	free(buffer);
+	if (len != lenw)
+		return (0);
+	return (lenw);
+}
