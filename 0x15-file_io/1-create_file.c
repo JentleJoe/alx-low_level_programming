d2e15e8dc40bd6b0a8686e0ff5247dce8efabd05
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-07-29 09:30:03 +0100

    updated source code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (n_letters = 0; text_content[nletters]; n_letters++)
+	for (n_letters = 0; text_content[n_letters]; n_letters++)
 		;
 
 	rwr = write(fd, text_content, n_letters);
