626cc47a0a84cba95c925fdc66fba7c0c90c68a0
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-07-29 10:44:40 +0100

    added source code for elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+
 
 int _putchar(char c);
 
@@ -21,6 +23,19 @@ int append_text_to_file(const char *filename, char *text_content);
 char *createBuf(char *file);
 
 void fileClose(int fd);
+
+/** FUNCTIONS FOR ELF */
+void checkElf(unsigned char *e_ident);
+void printMagic(unsigned char *e_ident);
+void printClass(unsigned char *e_ident);
+void printData(unsigned char *e_ident);
+void printVersion(unsigned char *e_ident);
+void printAbi(unsigned char *e_ident);
+void printOsabi(unsigned char *e_ident);
+void printType(unsigned int e_type, unsigned char *e_ident);
+void printEntry(unsigned long int e_entry, unsigned char *e_ident);
+void closeElf(int elf);
+
 
 
 #endif
